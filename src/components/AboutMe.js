eafbc0db1f0e4909a324f30a2d2cb26fea6e1743
Darby Johnson <darbyjjohnson1@gmail.com>
2022-03-26 14:50:11 -0400

    got the ContactMe, background image, and links working

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,7 +7,7 @@ function AboutMe() {
 
     return(
     <div>
-      <Card>
+      <Card style={{padding:"25px"}}>
       <Card.Body style= {{backgroundColor: "#f3edf2", fontSize: "20px", padding:"25px"}}>Hi! {<br/>} I'm Darby Johnson. I'm currently enrolled in the Georgia Tech Full Stack coding program set to graduate April 2022. I am looking for a software engineering position to kick start my future coding career!
       I am constantly evolving, growing, and learning more and more about the intracacies of coding and feel like I can be a good fit at your company. I am a fast and eager learner. <a href={resume}>Feel free to take a look at my resume!</a></Card.Body>
      </Card>
--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,12 +1,29 @@
 import React, { useState } from 'react';
 import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem, Container, Form, FormControl, Offcanvas } from 'react-bootstrap';
+import resume from '../Darby_Johnson.jpg';
 
 function ContactMe() {
     const [header, setHeader] = useState('Darby Johnson');
 
     return(
         <div>
-        <h1>Hi</h1>
+    <Nav style={{fontSize:"25px", border:'1px black solid', marginBottom:"50px", marginTop:"50px", backgroundColor: "white"}} defaultActiveKey="/home" as="ul">
+        <Nav.Item as="li">
+        <Nav.Link style={{color: "black"}}>Contact Me:</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+        <Nav.Link style={{color: "pink"}} href="mailto:darbyj1707@yahoo.com" eventKey="link-1">Send Email</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+        <Nav.Link style={{color: "pink"}} href="https://www.linkedin.com/in/darby-johnson-91936a91/" target="_blank" eventKey="link-1">LinkedIn</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+        <Nav.Link style={{color: "pink"}} href="https://github.com/darbyjenks"target="_blank" eventKey="link-1">GitHub</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+        <Nav.Link style={{color: "pink"}} href='https://darbyjenks.github.io/Darby-Johnson-Portfolio/' target="_blank" eventKey="link-1">Website</Nav.Link>
+        </Nav.Item>
+    </Nav>
     </div>
     )
 }
